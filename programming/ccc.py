5b6d68e9e95084f186d14cc30f68a7b74e315732
Sarveshwar Senthil Kumar <47092652+SarveshwarSenthilKumar@users.noreply.github.com>
2026-02-05 00:01:14 -0500

    Added comments to explain the code

--- programming/ccc.py
+++ programming/ccc.py
@@ -8,6 +8,7 @@ gold=0
 #CCC S2 2022
 
 matrix=[]
+# Initialize the matrix with "B"
 for i in range(int(rows)):
     row=[]
     for x in range(int(columns)):
@@ -16,23 +17,33 @@ for i in range(int(rows)):
 
 for i in range(int(inputs)):
     character, index = input().split()
+    # Check if character is R for row
     if character=="R":
+        # For each column
         for i in range(columns):
+            # If the value at the index is B, change it to G and increase gold count
             if matrix[int(index)-1][i]=="B":
                 matrix[int(index)-1][i]="G"
                 gold+=1
+            # If the value at the index is G, change it to B and decrease gold count
             else:
                 matrix[int(index)-1][i]="B"
                 gold-=1
+    
+    # Check if character is C for column
     elif character=="C":
+        # For each row
         for i in range(rows):
+            # If the value at the index is B, change it to G and increase gold count
             if matrix[i][int(index)-1]=="B":
                 matrix[i][int(index)-1]="G"
                 gold+=1
+            # If the value at the index is G, change it to B and decrease gold count
             else:
                 matrix[i][int(index)-1]="B"
                 gold-=1
 
 #Possible solution: use substring to change strings using multiplication of strings and slicing a single string variable as opposed to a multi dimensional array bringing down memory and time complexity
 
+#Output the amount of gold
 print(gold)
